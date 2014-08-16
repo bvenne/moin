ddd8d8d6338c258e83fe46c04eacdd9eef0d39e2
bvenne <bastian.vennemann@googlemail.com>
2014-08-16 17:46:55 +0200

    contacts test

--- js/index.js
+++ js/index.js
@@ -15,11 +15,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        
-        //funktion aufruf contacts (emails ) einlesen
-        getContactList();
-        
+        app.receivedEvent('deviceready');        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
