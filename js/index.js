20354017a84f2774bcadc212a4c6a50db2f93a4a
bvenne <Bastian.Vennemann@googlemail.com>
2014-08-18 16:13:39 +0200

    readcontact

--- js/index.js
+++ js/index.js
@@ -17,11 +17,21 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');     
 
+/*
 		var myContact = navigator.contacts.create({"displayName": "Test User"});
         myContact.note = "This contact has a note.";
         alert("The contact, " + myContact.displayName + ", note: " + myContact.note);
         app.receivedEvent('contactcreated');
         myContact.save(app.onContactSaveSuccess,app.onContactSaveError);
+*/
+
+		// find all contacts
+		var myContacts = new ContactFindOptions();
+		myContacts.filter = "";
+		var filter = ["displayName", "addresses"];
+		navigator.contacts.find(filter, app.onReadContactsSuccess, app.onReadContactsError, options);
+		app.receivedEvent('contactcreated');
+		
    
         
            
@@ -54,7 +64,24 @@ onContactSaveSuccess: function(contact) {
         window.alert("Save Failed");
     },    
 
-
+// read contacts
+onReadContactsSuccess: function(contacts) {
+			for (var i = 0; i < contacts.length; i++) {
+			for (var j = 0; j < contacts[i].addresses.length; j++) {
+				alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
+					"Type: "           + contacts[i].addresses[j].type          + "\n" +
+					"Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+					"Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+					"Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+					"Region: "         + contacts[i].addresses[j].region        + "\n" +
+					"Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+					"Country: "        + contacts[i].addresses[j].country);
+			}
+		}
+    },
+    onReadContactsError: function(contacts) {
+        window.alert("Read Failed", contact);
+    },    
 
 
 
